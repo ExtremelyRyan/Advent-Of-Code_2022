4219199d0911157f155f268345d5939bdeba2ea4
1140982 <ryan.e.mason@raytheon.com>
2022-12-22 11:44:59 -0600

    day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,164 +1,52 @@
-// --- Day 7: No Space Left On Device ---
-use std::{cell::RefCell, collections::HashMap, fs::read_to_string, path::Path, rc::Rc};
+use std::{fs::File, io::{BufReader, BufRead, Lines}};
 
-#[derive(Default)]
-pub struct Aoc2022_07 {
-    root: Rc<Dir>,
-}
+fn directory_size(reader: &mut Lines<BufReader<File>>, list: &mut Vec<i32>) -> i32 {
+    let mut size: i32 = 0;
 
-#[derive(Default, Debug)]
-struct Dir {
-    _name: String,
-    size: RefCell<usize>,
-    parent: Option<Rc<Dir>>,
-    subdir: RefCell<HashMap<String, Rc<Dir>>>,
-}
+    loop {
+        let line = reader.next();
+        match line {
+            None => break,
+            _ => (),
+        };
 
-impl Dir {
-    fn get_size(&self) -> usize {
-        *self.size.borrow()
-            + self
-                .subdir
-                .borrow()
-                .values()
-                .fold(0, |a, b| a + b.get_size())
-    }
-}
-
-fn read_lines<T: AsRef<Path>>(pathname: T) -> Vec<String> {
-    read_to_string(pathname)
-        .expect("unable to open file")
-        .split('\n')
-        .filter(|s| !s.is_empty())
-        .map(|s| s.to_string())
-        .collect()
-}
-
-fn main() {
-    println!("DAY 7 NOT COMPLETE");
-    // let mut aoc = Aoc2022_07{root : Rc::new(Dir::default())};
-    // let (p1, p2) = aoc.p1_p2();
-}
-
-pub trait Run { 
-    fn name(&self) -> (usize, usize);
-    fn parse(&mut self);
-    fn part1(&mut self) -> usize;
-    fn part2(&mut self) -> usize;
-    fn p1_p2 (&mut self) -> (usize, usize);
-}
-
-impl Run for Aoc2022_07 { 
-    fn name(&self) -> (usize, usize) {
-        (2022, 7)
-    }
-
-    fn parse(&mut self) {
-        let mut cwd = Rc::clone(&self.root);
-        for line in read_lines("./input/day7_input.txt") {
-            let words = line.split(' ').collect::<Vec<&str>>();
-            //println!("words: {words:?}");
-            match (words[0], words[1]) {
-                ("$", "ls") => {}
-                ("$", "cd") => match words[2] {
-                    "/" => cwd = Rc::clone(&self.root),
-                    ".." => cwd = Rc::clone(cwd.parent.as_ref().unwrap()),
-                    dirname => {
-                       // println!("cwd: {:?}", cwd.subdir);
-                        let newdir: Rc<Dir> = cwd.subdir.borrow().get(dirname).unwrap().clone();
-                        cwd = newdir;
-                    }
-                },
-                ("dir", dirname) => {
-                    cwd.subdir.borrow_mut().insert(
-                        dirname.to_string(),
-                        Rc::new(Dir {
-                            _name: dirname.to_string(),
-                            size: RefCell::new(0),
-                            parent: Some(Rc::clone(&cwd)),
-                            subdir: RefCell::new(HashMap::new()),
-                        }),
-                    );
-                }
-                (size, _name) => {
-                    *cwd.size.borrow_mut() += size.parse::<usize>().unwrap();
-                }
-            }
+        let line = line.unwrap().unwrap();
+        if line == "$ cd .." {
+            break;
         }
-    }
-
-    fn part1(&mut self) -> usize {
-        let mut to_visit = vec![Rc::clone(&self.root)];
-        let mut total = 0;
-
-        while let Some(dir) = to_visit.pop() {
-            to_visit.extend(dir.subdir.borrow().values().map(Rc::clone));
-
-            let size = dir.get_size();
-            if size <= 100000 {
-                total += size;
-            }
+        let words: Vec<&str> = line.split(" ").collect();
+        if words[1] == "cd" {
+            size += directory_size(reader, list);
         }
-       // println!("total {}", total);
-        let p2 = self.part2();
-       // println!("part two {}", p2);
-        total
-
+        size += words[0].parse::<i32>().unwrap_or(0);
     }
+    list.push(size);
 
-    fn part2(&mut self) -> usize {
-        //println!("debug: root {0:?}", self.root);
-        let total_size = self.root.get_size();
-        let free_space = 70000000 - total_size;
-        let space_needed = 30000000 - free_space;
+    size
+}
 
-        let mut to_visit = vec![Rc::clone(&self.root)];
-        let mut best = usize::MAX;
+fn main() -> std::io::Result<()> {
+    let file = File::open("./input/day7_input.txt")?;
+    let mut reader = BufReader::new(file).lines();
+    let mut directories: Vec<i32> = Vec::new();
+    let total = directory_size(&mut reader, &mut directories);
 
-        while let Some(dir) = to_visit.pop() {
-            to_visit.extend(dir.subdir.borrow().values().map(Rc::clone));
+    let part1: i32 = directories.iter()
+    .fold(0, | acc, directory | {
+        acc + if *directory <= 100000 { *directory } else { 0 }
+    });
 
-            let size = dir.get_size();
-            if size >= space_needed {
-                best = best.min(size);
-            }
-        }
-        //println!("best {}", best);
-        best
-    }
+    let upgrade_size = 30000000;
+    let max_space = 70000000;
+    let target_space = upgrade_size - (max_space - total);
 
-    fn p1_p2 (&mut self) -> (usize, usize) {
+    let part2: i32 = directories.iter()
+    .fold(total, | acc, directory | {
+        if *directory <= acc && *directory >= target_space { *directory } else { acc }
+    });
 
-        let mut to_visit = vec![Rc::clone(&self.root)];
-        let mut total = 0;
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 
-        while let Some(dir) = to_visit.pop() {
-            to_visit.extend(dir.subdir.borrow().values().map(Rc::clone));
-
-            let size = dir.get_size();
-            if size <= 100000 {
-                total += size;
-            }
-        }  
-        
-
-        //println!("debug: root {0:?}", self.root);
-        let total_size = self.root.get_size();
-        let free_space = 70000000 - total_size;
-        let space_needed = 30000000 - free_space;
-
-        let mut to_visit = vec![Rc::clone(&self.root)];
-        let mut best = usize::MAX;
-
-        while let Some(dir) = to_visit.pop() {
-            to_visit.extend(dir.subdir.borrow().values().map(Rc::clone));
-
-            let size = dir.get_size();
-            if size >= space_needed {
-                best = best.min(size);
-            }
-        }
-       // println!("total: {0}, best {1}",total, best);
-        (total ,best)
-    }
+    Ok(())
 }
